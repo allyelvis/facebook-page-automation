6e28904d1dd49ba8cb232ad416efaf419a8731ba
allyelvis <allyelvis6569@gmail.com>
2025-01-05 16:51:10 +0000

    appjs

---
+++ generators/app_generator/build/app.js
@@ -0,0 +1,14 @@
+function startAppGenerator() {
+    alert('Launching App Generator...');
+    window.location.href = "/app_generator";
+}
+
+function startMusicGenerator() {
+    alert('Launching Music Generator...');
+    window.location.href = "/music_generator";
+}
+
+function startMovieGenerator() {
+    alert('Launching Movie Generator...');
+    window.location.href = "/movie_generator";
+}
